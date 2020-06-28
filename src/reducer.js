421d1979c99cda7b7f9c5b2ea89e5b3374c54992
Edisson Mata <edisson.mata@pitech.com.do>
2020-06-28 01:15:42 -0400

    completed

--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,5 @@
 const reducer = (state, action) => {
     switch (action.type) {
-        case 'change_color':
-            return {...state, layoutColor: action.payload}
         default:
             return state
     }
