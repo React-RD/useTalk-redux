75ce4ef6eabbf89315cd756eee427d98d02560cd
Edisson Mata <edisson.mata@pitech.com.do>
2020-06-28 01:11:30 -0400

    The example has been completed

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,7 @@
 const reducer = (state, action) => {
     switch (action.type) {
+        case 'change_color':
+            return {...state, layoutColor: action.payload}
         default:
             return state
     }
---
+++ src/utils/consts.js
@@ -0,0 +1,12 @@
+export const colors = [
+    'bg-violet',
+    'bg-red',
+    'bg-blue',
+    'bg-green',
+    'bg-pink',
+    'bg-ocean',
+    'bg-turquoise',
+    'bg-swamp',
+    'bg-orange',
+    'bg-yellow'
+]
