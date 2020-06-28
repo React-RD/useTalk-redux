18d4983c4b7d173229cc9240cacc35cdf249531b
Edisson Mata <edisson.mata@pitech.com.do>
2020-06-28 01:17:20 -0400

    Revert "completed"
    This reverts commit 421d1979

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
+import AwesomeButton from "./components/AwesomeButton";
+import {useDispatch, useSelector} from 'react-redux'
+import { colors } from './utils/consts'
 
 const App = () => {
-    return (
-        <div>
+    const dispatcher = useDispatch()
+    const bgColor = useSelector(state => state.layoutColor)
 
+    const handleChangeColor = () => {
+        const randomIndex = Math.floor((Math.random() * 9) + 1);
+        const colorToSet = colors[randomIndex]
+
+        dispatcher({
+            type: 'change_color',
+            payload: colorToSet
+        })
+    }
+
+    return (
+        <div className={'app center-flex '}>
+            <AwesomeButton bgColor={bgColor} onClick={handleChangeColor} />
         </div>
     )
 }
